6e88dfb6a0ca7d38ab5da4db1b312ffb6b9b1e24
MannuVilasara <mannuvilasara@gmail.com>
2025-05-07 19:36:28 +0530

    fix: 👾 Format

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import json from '@rollup/plugin-json'; 
-import tailwindcss from '@tailwindcss/vite';
-import envCompatible from 'vite-plugin-env-compatible';
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import json from "@rollup/plugin-json";
 import tailwindcss from "@tailwindcss/vite";
->>>>>>> 114fbd4df86c09a966a60c53848324cddd47e54d
+import envCompatible from "vite-plugin-env-compatible";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    envCompatible(),
-    tailwindcss(),
-  ],
+  plugins: [react(), envCompatible(), tailwindcss()],
   server: {
     allowedHosts: [
       "localhost",
